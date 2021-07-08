bbdf3f3db696badd8811b84be70a0598eb2ab275
dengbaole <1550555930@qq.com>
2021-07-09 07:59:22 +0800

    update

--- Static digital tube display 1234567/main.c
+++ Static digital tube display 1234567/main.c
@@ -3,24 +3,65 @@
 typedef unsigned char u8;
 typedef unsigned int u16;
 
-sbit LSA2 = P2^2;
-sbit LSA3 = P2^3;
-sbit LSA4 = P2^4;
+sbit LSA = P2^2;
+sbit LSB = P2^3;
+sbit LSC = P2^4;
 
 u8 code smgduan[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,
                    0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
 //分别表示0123456789abcdef
-//code是用来减少数据大小
+									 
+void delay(u16 i)
+{
+	while(i--);
+}	
 
 
+void Digdisplay()
+{
+	u8 i;
+	for(i= 0;i<8;i++)
+	{
+		//分别点亮八个数码管
+		switch(i)
+		{
+			case(0):
+				LSA = 0;LSB = 0; LSC = 0;
+				break;
+			case(1):
+				LSA = 1;LSB = 0; LSC = 0;
+				break;
+			case(2):
+				LSA = 0;LSB = 1; LSC = 0;
+				break;
+			case(3):
+				LSA = 1;LSB = 1; LSC = 0;
+				break;
+			case(4):
+				LSA = 0;LSB = 0; LSC = 1;
+				break;
+			case(5):
+				LSA = 1;LSB = 0; LSC = 1;
+				break;
+			case(6):
+				LSA = 0;LSB = 1; LSC = 1;
+				break;
+			case(7):
+				LSA = 1;LSB = 1; LSC = 1;
+				break;
+		}
+		//对点亮的数码管赋值
+		P0 = smgduan[i];
+		delay(100);
+		P0 = 0x00;
+	}
+}
+									 
 int main()
 {
-	LSA2 = 0;
-	LSA3 = 0;
-	LSA4 = 0;
-	P0 = smgduan[0];
-	
-	
-	return 0;
-	
+	while(1)
+	{
+		Digdisplay();
+	}
+	 
 }
